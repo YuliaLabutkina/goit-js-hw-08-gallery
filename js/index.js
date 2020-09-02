03dc819bc6baf51fbf24d309b358281e4d1e1532
Labutkina Yulia <labutkinayuliaa@gmail.com>
2020-09-02 17:16:36 +0300

    added swipe photo

--- js/index.js
+++ js/index.js
@@ -8,8 +8,6 @@ const modalOpenRef = document.querySelector('div.lightbox');
 const buttonCloseRef = document.querySelector('button[data-action="close-lightbox"]');
 const boxCloseRef = document.querySelector('div.lightbox__content');
 const fragment = document.createDocumentFragment();
-const arrayPhotos = [];
-
 
 
 function createElementgallery(gallery) {
@@ -23,11 +21,11 @@ function createElementgallery(gallery) {
         element.classList.add("gallery__item");
         link.classList.add("gallery__link");
         link.setAttribute("href", `${el.original}`);
-        link.setAttribute("data-index", `${index}`);
         img.classList.add("gallery__image");
         img.setAttribute("src", `${el.preview}`);
         img.setAttribute("data-source", `${el.original}`);
         img.setAttribute("alt", `${el.description}`);
+        img.setAttribute("data-index", `${index}`);
 
         link.appendChild(img);
         element.appendChild(link);
@@ -38,8 +36,8 @@ function createElementgallery(gallery) {
 };
 
 createElementgallery(gallery);
-const galleryLinkRef = document.querySelector('.gallery__link');
-console.log(galleryLinkRef);
+
+const galleryLinkRef = document.querySelectorAll('.gallery__image');
 
 
 galleryListRef.addEventListener('click', onGalleryClick);
@@ -56,19 +54,25 @@ function onGalleryClick(event) {
 
     const imageRef = event.target;
     const url = imageRef.dataset.source;
-    openModal(url);
+    openModal(url, event.target);
 };
 
-function openModal(url) {
+function openModal(url, el) {
     window.addEventListener('keydown', closeESC);
-    window.addEventListener('keydown', onScrollLeftRight);
+    window.addEventListener('keydown', onKeydownLeftRight);
+    
+    el.classList.add('js-active');
     imgLargeRef.src = url;
     modalOpenRef.classList.toggle('is-open');
 }
 
 function closeModal() {
     window.removeEventListener('keydown', closeESC);
-    window.removeEventListener('keydown', onScrollLeftRight);
+    window.removeEventListener('keydown', onKeydownLeftRight);
+
+    const galleryImgRemoveRef = document.querySelector('.js-active');
+    galleryImgRemoveRef.classList.remove('js-active');
+
     imgLargeRef.src = '';
     modalOpenRef.classList.toggle('is-open');
 }
@@ -79,23 +83,22 @@ function closeESC(event) {
     }
 }
 
-function onScrollLeftRight(event) {
+function onKeydownLeftRight(event) {
 
-    if(event.code === 'ArrowRight') {
+    const activeImgRef = document.querySelector('.js-active');
+    let index = +activeImgRef.getAttribute('data-index');
 
-        let index = +galleryLinkRef.getAttribute('data-index');
+    if(event.code === 'ArrowRight' && index < 8) {
         index += 1;
-        imgLargeRef.src = arrayPhotos[index];
+        imgLargeRef.src = gallery[index].original;
+        galleryLinkRef[index].classList.add('js-active');
+        activeImgRef.classList.remove('js-active');
+    }
 
+    if(event.code === 'ArrowLeft' && index > 0) {
+        index -= 1;
+        imgLargeRef.src = gallery[index].original;
+        galleryLinkRef[index].classList.add('js-active');
+        activeImgRef.classList.remove('js-active');
     }
 }
-
-
-function arrayOfPhotos(gallery) {
-    const array = gallery.map(el => {
-        arrayPhotos.push(el.original);
-    });
-    console.log(arrayPhotos);
-}
-
-arrayOfPhotos(gallery);
